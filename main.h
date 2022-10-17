b517a6f55aceaae6008519fe1d437353a9f7a962
Sanbruce1A <saniimoleayo@gmail.com>
2022-10-17 13:01:40 -0700

    Task Printf

--- main.h
+++ main.h
@@ -52,8 +52,7 @@ typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
 
 int handle_print(const char *fmt, int *i,
-
-				 va_list list, char buffer[], int flags, int width, int precision, int size);
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
 
 /****************** FUNCTIONS ******************/
@@ -106,20 +105,9 @@ int print_hexa_upper(va_list types, char buffer[],
 
 
 int print_hexa(va_list types, char map_to[],
-
-
-
-			  char buffer[], int flags, char flag_ch, int width, int precision, int size);
-
-
-
-
-
-
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 /* Function to print non printable characters */
-
-
 
 int print_non_printable(va_list types, char buffer[],
 
@@ -246,24 +234,11 @@ int write_pointer(char buffer[], int ind, int length,
 
 
 int write_unsgnd(int is_negative, int ind,
+char buffer[],
 
-
-
-				 char buffer[],
-
-
-
-				 		 int flags, int width, int precision, int size);
-
-
-
-
-
-
+int flags, int width, int precision, int size);
 
 /****************** UTILS ******************/
-
-
 
 int is_printable(char);
 
